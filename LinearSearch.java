86d4382b379e38b89012fa36db25ac1956033b21
Divyanshi Singhal <95673161+Divyanshi2707@users.noreply.github.com>
2022-02-01 04:05:26 -1200

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String args[]) {
+        int count, elements, integer[], search_value;
+        Scanner lsObj = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        elements = lsObj.nextInt();
+
+        integer = new int[elements];
+        System.out.print("Enter " +elements+ " Integer");
+        for (count = 0; count < elements; count++) {
+            integer[count] = lsObj.nextInt();
+        }
+
+        System.out.print("Enter the search value: ");
+        search_value = lsObj.nextInt();
+
+        for (count = 0; count < elements; count++) {
+            if (integer[count] == search_value) {
+                System.out.print(search_value + " is present at location " +(count+1));
+                break;
+            }
+        }
+        if (count == elements)
+            System.out.println(search_value+ " doesn't exist in array.");
+    }
+}
